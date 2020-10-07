0dda34c128c18dcf2be182dd52f23c7ee291d226
Torsten Pause <torsten.pause@student.kyh.se>
2020-10-07 13:05:46 +0200

    no message

--- src/com/company/YatziTest.java
+++ src/com/company/YatziTest.java
@@ -1,29 +1,28 @@
 package com.company;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class YatziTest {
+
     @Test
+    @DisplayName("testing when all dices are the same")
     void isYatziWhenAllDiceMatches() {
-        Die[] dice = new Die[5];
-        for(Die die: dice) {
-            die.value = 6;
-        }
-        //Assert something?
+        int[] dice = {6,6,6,6,6};
+        for(int content : dice) { assertEquals(6, content, "Not six"); }
+
     }
 
     @Test
+    @DisplayName("testing when dice are not all equal")
     void isNotYatziWhenOneDieIsNotMatchingTheOther() {
         Die[] dice = new Die[5];
         for(Die die: dice) {
             die.value = 6;
         }
         dice[5].value = 1;
-        //Assert something?
+        for(Die die: dice) { assertEquals(6, die.value, "Not six"); }
     }
 }
